4a1b1870db1356e11284d3bcd43b5f64c26e17cf
Jem Herrera <jhneramuz@gmail.com>
2024-12-09 18:35:04 -0700

    delete getMostRecent4pmet

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import { record } from "./helpers/record";
 (async () => {
   const DOWN_10 = "v=171&f=geo_usa,ta_change_d10&ft=3";
   const UP_10 = "v=171&f=geo_usa,ta_change_u10";
-
   await record(DOWN_10);
   await record(UP_10);
 })();
