fea43c1bdea724020bddb31a99bdb9fae6427df4
Jem Herrera <jhneramuz@gmail.com>
2025-04-03 17:57:54 -0600

    Update filters

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { record } from "./helpers/record";
 
 (async () => {
-  const BASE_SCREENER =
-    "v=171&f=geo_usa,ta_perf_4wup,ta_perf2_ytdup,ta_sma50_pa,ta_volatility_mo2&ft=3";
+  const BASE_SCREENER = "v=171&f=geo_usa,ta_perf2_ytdup&ft=3";
+
   await record(BASE_SCREENER);
 })();
